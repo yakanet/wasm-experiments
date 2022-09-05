52e2f1e67c90758d711f48916f4c459083360005
mbroutin <mbroutin@sedona.fr>
2022-09-05 07:19:49 +0200

    clean(css): use global style instead of editing app.html

---
+++ svm-editor/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<slot />
+
+<style global>
+    @import url("https://fonts.googleapis.com/css2?family=Open+Sans&display=swap");
+
+    html {
+        font-family: "Open Sans", sans-serif;
+    }
+
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+    }
+
+    body {
+        padding: 0;
+        margin: 0;
+    }
+</style>
