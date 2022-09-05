682fd62ac72dca99c1b5136be6d4460973b60121
yakanet <yakanet@gmail.com>
2022-09-05 06:04:27 +0000

    Deploying to gh-pages from @ yakanet/wasm-experiments@bcc5211948e3117aa66d07b810bca416e8f40ba5 🚀

--- _app/immutable/chunks/1-24b31730.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/error.svelte-281ee134.js";import"./index-aaceca5f.js";import"./singletons-de751b83.js";export{r as component};
---
+++ _app/immutable/chunks/1-8532bbe0.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/error.svelte-95e1656d.js";import"./index-aaceca5f.js";import"./singletons-68a3cb92.js";export{r as component};
